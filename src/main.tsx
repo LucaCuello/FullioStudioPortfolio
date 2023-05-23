e208c3c16f1458afe40bfda58b0738e1c9c5d349
Luca <lucagcuello@gmail.com>
2023-05-22 21:06:47 -0300

    Font importation

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <h1 className="website-title">Fullio Studio</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolorum
+      officia pariatur atque saepe aliquam illum adipisci doloremque facere dolorem?
+    </p>
+  </React.StrictMode>
 );
