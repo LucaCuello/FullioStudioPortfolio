3b2c046a89b20f5889804f32e9a0293d623ade1d
Luca <lucagcuello@gmail.com>
2023-05-24 01:15:39 -0300

    Integrated React Router

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Header } from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Header />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
