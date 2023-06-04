b12289a9b937ea8484417f1287ba9a445b14cfb7
Luca <lucagcuello@gmail.com>
2023-06-04 00:55:35 -0300

    Added burguer menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,51 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 export const Header = () => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    if (active) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [active]);
+
   return (
     <header className="website-header">
       <div className="logo-container">
         <span className="website-logo">Fullio Studio</span>
       </div>
-      <nav className="website-nav">
+      {/* Hamburguer menu */}
+      <div
+        className={active ? "hamburguer-menu hamburguer-menu-active" : "hamburguer-menu"}
+        onClick={() => setActive(!active)}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <nav className={active ? "website-nav website-nav-active" : "website-nav"}>
         <ul>
           <li>
-            <NavLink to="/home" className="item-link">
+            <NavLink to="/home" className="item-link" onClick={() => setActive(false)}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/aboutme" className="item-link">
+            <NavLink to="/aboutme" className="item-link" onClick={() => setActive(false)}>
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to="/projects" className="item-link">
+            <NavLink to="/projects" className="item-link" onClick={() => setActive(false)}>
               Projects
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className="item-link">
+            <NavLink to="/contact" className="item-link" onClick={() => setActive(false)}>
               Contact
             </NavLink>
           </li>
